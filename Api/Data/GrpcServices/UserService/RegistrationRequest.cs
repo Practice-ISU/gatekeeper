31878b39540e9fbd8d0fbe52f16f61d8ec8b6805
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-21 13:13:10 +0800

    Create RegistrationRequest to UserService

---
+++ Api/Data/GrpcServices/UserService/RegistrationRequest.cs
@@ -0,0 +1,20 @@
+﻿using Api.Data.Api.Requests;
+using Grpc.Net.Client;
+using ProtoUser;
+
+namespace Api.Data.GrpcServices.UserService
+{
+    public class RegistrationRequestGrpc
+    {
+        public static async Task<UserRegisterResponse> Registration(RegistrationRequest model, string chanel)
+        {
+            using var channel = GrpcChannel.ForAddress(chanel);
+            var client = new UserGrpcService.UserGrpcServiceClient(channel);
+            var response = await client.RegisterUserAsync(new UserRegisterDTO { Username = model.Username, Password = model.Password });
+            
+            // Console.WriteLine($"Success: {response.Details.Success}, Username: {response.User.Username}");
+            
+            return response;
+        }
+    }
+}

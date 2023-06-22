d80d0bf2cb8a590a2bc82975f2ff7dfbfd860dd0
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-22 22:49:21 +0800

    Create Login Request/Response for API

---
+++ Api/Data/Api/Responses/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Data.Api.Responses
+{
+    public class LoginResponse
+    {
+        public string? Token { get; set; }
+        public string? Message { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}

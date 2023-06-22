b2377c3117599bd957ab350a3a621de17417fb89
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-22 12:08:11 +0800

    Create client methods for discovery-service

---
+++ Api/Data/GrpcServices/DiscoveryService/GetChanel.cs
@@ -0,0 +1,24 @@
+﻿using Discovery;
+using Grpc.Net.Client;
+
+namespace Api.Data.GrpcServices.DiscoveryService
+{
+    public class GetChannelRequest
+    {
+        private static readonly string _channel = ConfigurationHelper.GetDiscoveryChannel();
+
+        public static async Task<string>? GetChannel(string serviceName)
+        {
+            using var channel = GrpcChannel.ForAddress(_channel);
+            var client = new Discovery.DiscoveryService.DiscoveryServiceClient(channel);
+            var response = await client.GetChannelAsync(new ServiceNameRequest { ServiceName = serviceName });
+
+            if (response.Status == ServiceInfo.Types.Status.Down)
+            {
+                throw new Exception("Service not available");
+            }
+
+            return response.Channel;
+        }
+    }
+}
---
+++ Api/Data/GrpcServices/DiscoveryService/GetServices.cs
@@ -0,0 +1,21 @@
+﻿using Discovery;
+using Grpc.Net.Client;
+using ProtoUser;
+using System.Threading.Channels;
+using Microsoft.Extensions.Configuration;
+
+namespace Api.Data.GrpcServices.DiscoveryService
+{
+    public class GetServicesRequest
+    {
+        private static readonly string _channel = ConfigurationHelper.GetDiscoveryChannel();
+        public static async Task<GetServicesResponse> GetServices()
+        {
+            using var channel = GrpcChannel.ForAddress(_channel);
+            var client = new Discovery.DiscoveryService.DiscoveryServiceClient(channel);
+            var response = await client.GetServicesAsync(new Discovery.GetServicesRequest { });
+            
+            return response;
+        }
+    }
+}

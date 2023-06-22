48ec5323d4c3b3dc7664439e995906df6f1b19b0
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-22 22:59:05 +0800

    Create VerifyUser grpc

---
+++ Api/Data/GrpcServices/UserService/VerifyUser.cs
@@ -0,0 +1,35 @@
+﻿using Api.Data.Api.Requests;
+using Grpc.Core;
+using Grpc.Net.Client;
+using ProtoUser;
+
+namespace Api.Data.GrpcServices.UserService
+{
+    public class VerifyUserGrpc
+    {
+        public static async Task<UserVerifyResponse> VerifyUser(string token, string channel)
+        {
+            ChannelBase? grpcChannel = null;
+            try
+            {
+                grpcChannel = GrpcChannel.ForAddress(channel);
+                var grpcClient = new UserGrpcService.UserGrpcServiceClient(grpcChannel);
+
+                var response = await grpcClient.VerifyUserAsync(new UserToken
+                {
+                    Token = token
+                });
+
+                return response;
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.NotFound)
+            {
+                throw new ArgumentException($"Invalid channel url: {channel}", nameof(channel));
+            }
+            finally
+            {
+                await grpcChannel?.ShutdownAsync()!;
+            }
+        }
+    }
+}

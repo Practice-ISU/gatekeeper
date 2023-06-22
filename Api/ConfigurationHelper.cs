c15c65a4998e7d07b5eddb9f7aa3d5ff75dce9fd
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-22 12:06:34 +0800

    Create ConfigurationHelper

---
+++ Api/ConfigurationHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace Api
+{
+    using Microsoft.Extensions.Configuration;
+
+    public static class ConfigurationHelper
+    {
+        private static readonly IConfiguration Configuration;
+
+        static ConfigurationHelper()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public static string GetDiscoveryChannel()
+        {
+            string? discoveryChannel = Configuration.GetValue<string>("DiscoveryChanel");
+            if (discoveryChannel == null)
+            {
+                throw new InvalidOperationException("\"DiscoveryChanel\" not in config (appsettings.json)");
+            }
+
+            return discoveryChannel;
+        }
+
+    }
+}

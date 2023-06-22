faac71c4e5dc0e4355121358411bdfc456ba91f7
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-22 22:48:57 +0800

    Create LoginRequestGrpc

---
+++ Api/Data/GrpcServices/UserService/LoginRequest.cs
@@ -0,0 +1,36 @@
+﻿using Api.Data.Api.Requests;
+using Grpc.Core;
+using Grpc.Net.Client;
+using ProtoUser;
+
+namespace Api.Data.GrpcServices.UserService
+{
+    public class LoginRequestGrpc
+    {
+        public static async Task<UserLoginResponse> Login(LoginRequest model, string channel)
+        {
+            ChannelBase? grpcChannel = null;
+            try
+            {
+                grpcChannel = GrpcChannel.ForAddress(channel);
+                var grpcClient = new UserGrpcService.UserGrpcServiceClient(grpcChannel);
+
+                var response = await grpcClient.LoginUserAsync(new UserLoginDTO
+                {
+                    Username = model.Username,
+                    Password = model.Password
+                });
+
+                return response;
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.NotFound)
+            {
+                throw new ArgumentException($"Invalid channel url: {channel}", nameof(channel));
+            }
+            finally
+            {
+                await grpcChannel?.ShutdownAsync()!;
+            }
+        }
+    }
+}

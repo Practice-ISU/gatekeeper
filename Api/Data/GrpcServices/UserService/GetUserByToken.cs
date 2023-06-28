248f6beeef467bedb93d0f65dc686dc5c4934acb
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-28 22:58:07 +0800

    Create GetUserByTokenGrpc

---
+++ Api/Data/GrpcServices/UserService/GetUserByToken.cs
@@ -0,0 +1,40 @@
+﻿using Api.Data.Api.Requests;
+using Grpc.Core;
+using Grpc.Net.Client;
+using ProtoUser;
+
+namespace Api.Data.GrpcServices.UserService
+{
+    public class GetUserByTokenGrpc
+    {
+        public static async Task<UserResponse> GetUserByTokenAsync(string token, string channel)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                throw new ArgumentException("Token cannot be null or empty", nameof(token));
+            }
+
+            if (string.IsNullOrEmpty(channel))
+            {
+                throw new ArgumentException("Channel cannot be null or empty", nameof(channel));
+            }
+
+            using var grpcChannel = GrpcChannel.ForAddress(channel);
+            var grpcClient = new UserGrpcService.UserGrpcServiceClient(grpcChannel);
+
+            try
+            {
+                var response = await grpcClient.GetUserByTockenAsync(new UserToken
+                {
+                    Token = token
+                });
+
+                return response;
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.NotFound)
+            {
+                throw new InvalidOperationException($"Invalid channel URL: {channel}", ex);
+            }
+        }
+    }
+}

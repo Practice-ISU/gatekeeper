66543b10ff4f0bc2351110d652b004f390fe4768
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-28 22:57:58 +0800

    Create AddFolderRequestGrpc

---
+++ Api/Data/GrpcServices/FolderService/AddFolder.cs
@@ -0,0 +1,39 @@
+﻿using Api.Data.Api.Requests;
+using Discovery;
+using Folderservice;
+using Grpc.Core;
+using Grpc.Net.Client;
+using ProtoUser;
+using System.Threading.Channels;
+
+namespace Api.Data.GrpcServices.FolderService
+{
+    public class AddFolderRequestGrpc
+    {
+        public static async Task<FolderResponse> AddFolder(AddFolderRequest model, Int64 userId, string channel)
+        {
+            ChannelBase? grpcChannel = null;
+            try
+            {
+                grpcChannel = GrpcChannel.ForAddress(channel);
+                var grpcClient = new Folderservice.FolderService.FolderServiceClient(grpcChannel);
+
+                var response = await grpcClient.AddFolderAsync(new FolderAddDTO
+                {
+                    UserId = userId,
+                    FolderName = model.FolderName
+                });
+
+                return response;
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.NotFound)
+            {
+                throw new ArgumentException($"Invalid channel url: {channel}", nameof(channel));
+            }
+            finally
+            {
+                await grpcChannel?.ShutdownAsync()!;
+            }
+        }
+    }
+}
